fff10f43bde3eb60e261db97be3b4a3aa2f95ad9
alex <alex@slab.org>
2022-02-10 17:40:59 +0000

    Failing test for `when`

--- test/pattern.test.mjs
+++ test/pattern.test.mjs
@@ -178,6 +178,12 @@ describe('Pattern', function() {
     it('Never faster', function () {
       assert.equal(pure("a").when(pure(false), x => x._fast(2)).firstCycle.length, 1)
     })
+    it('Can alternate', function () {
+      assert.deepStrictEqual(
+        pure(10).when(slowcat(true,false),add(3)).fast(4).firstCycle,
+        slowcat(10,13,10,10).firstCycle
+      )
+    })
   })
   describe('fastcat()', function () {
     it('Can concatenate two things', function () {
