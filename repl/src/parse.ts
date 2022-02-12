67c8ac7d9d443c3427c97d30cd47a6a11a39b39e
Felix Roos <roos@entrecode.de>
2022-02-13 00:13:15 +0100

    make strudel classes available to eval

--- repl/src/parse.ts
+++ repl/src/parse.ts
@@ -20,8 +20,11 @@ const {
   polyrhythm,
   pr,
   /* reify, */ silence,
-  Fraction,
   timeCat,
+  Fraction,
+  Pattern,
+  TimeSpan,
+  Hap,
 } = strudel;
 const { autofilter, filter, gain } = toneStuff;
 
