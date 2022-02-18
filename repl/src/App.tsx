bad6cf0ed7a540777869692e2d9ecbfbbd3091da
Felix Roos <roos@entrecode.de>
2022-02-18 11:55:31 +0100

    encapsulate repl logic into hook

--- repl/src/App.tsx
+++ repl/src/App.tsx
@@ -1,15 +1,11 @@
-import React, { useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react';
-import logo from './logo.svg';
-import cx from './cx';
+import React, { useLayoutEffect, useRef } from 'react';
 import * as Tone from 'tone';
-import useCycle from './useCycle';
-import type { Pattern } from './types';
-import * as tunes from './tunes';
-import { evaluate } from './evaluate';
 import CodeMirror from './CodeMirror';
-import hot from '../public/hot';
-import { isNote } from 'tone';
-import { useWebMidi } from './midi';
+import cx from './cx';
+import { evaluate } from './evaluate';
+import logo from './logo.svg';
+import * as tunes from './tunes';
+import useRepl from './useRepl';
 
 // TODO: use https://www.npmjs.com/package/@monaco-editor/react
 
@@ -38,107 +34,15 @@ function getRandomTune() {
 const randomTune = getRandomTune();
 
 function App() {
-  const [code, setCode] = useState<string>(decoded || randomTune);
-  const [activeCode, setActiveCode] = useState<string>();
-  const [log, setLog] = useState('');
-  const logBox = useRef<any>();
-  const [error, setError] = useState<Error>();
-  const [pattern, setPattern] = useState<Pattern>();
-  const dirty = code !== activeCode;
-  const activateCode = (_code = code) => {
-    !cycle.started && cycle.start();
-    if (activeCode && !dirty) {
-      setError(undefined);
-      return;
-    }
-    try {
-      const parsed = evaluate(_code);
-      setPattern(() => parsed.pattern);
-      window.location.hash = '#' + encodeURIComponent(btoa(code));
-      setError(undefined);
-      setActiveCode(_code);
-    } catch (err: any) {
-      setError(err);
-    }
-  };
-  const pushLog = (message: string) => setLog((log) => log + `${log ? '\n\n' : ''}${message}`);
-  // logs events of cycle
-  const logCycle = (_events: any, cycle: any) => {
-    if (_events.length) {
-      pushLog(`# cycle ${cycle}\n` + _events.map((e: any) => e.show()).join('\n'));
-    }
-  };
-  // cycle hook to control scheduling
-  const cycle = useCycle({
-    onEvent: useCallback((time, event) => {
-      try {
-        if (!event.value?.onTrigger) {
-          const note = event.value?.value || event.value;
-          if (!isNote(note)) {
-            throw new Error('not a note: ' + note);
-          }
-          defaultSynth.triggerAttackRelease(note, event.duration, time);
-          /* console.warn('no instrument chosen', event);
-          throw new Error(`no instrument chosen for ${JSON.stringify(event)}`); */
-        } else {
-          const { onTrigger } = event.value;
-          onTrigger(time, event);
-        }
-      } catch (err: any) {
-        console.warn(err);
-        err.message = 'unplayable event: ' + err?.message;
-        pushLog(err.message); // not with setError, because then we would have to setError(undefined) on next playable event
-      }
-    }, []),
-    onQuery: useCallback(
-      (span) => {
-        try {
-          return pattern?.query(span) || [];
-        } catch (err: any) {
-          setError(err);
-          return [];
-        }
-      },
-      [pattern]
-    ),
-    onSchedule: useCallback((_events, cycle) => logCycle(_events, cycle), [pattern]),
-    ready: !!pattern,
+  const { setCode, setPattern, error, code, cycle, dirty, log, togglePlay } = useRepl({
+    tune: decoded || randomTune,
+    defaultSynth,
   });
-
-  // set active pattern on ctrl+enter
-  useLayoutEffect(() => {
-    const handleKeyPress = (e: any) => {
-      if (e.ctrlKey || e.altKey) {
-        switch (e.code) {
-          case 'Enter':
-            activateCode();
-            !cycle.started && cycle.start();
-            break;
-          case 'Period':
-            cycle.stop();
-        }
-      }
-    };
-    document.addEventListener('keypress', handleKeyPress);
-    return () => document.removeEventListener('keypress', handleKeyPress);
-  }, [pattern, code]);
-
+  const logBox = useRef<any>();
   // scroll log box to bottom when log changes
   useLayoutEffect(() => {
     logBox.current.scrollTop = logBox.current?.scrollHeight;
   }, [log]);
-
-  useWebMidi({
-    ready: useCallback(({ outputs }) => {
-      pushLog(`WebMidi ready! Just add .midi(${outputs.map((o) => `"${o.name}"`).join(' | ')}) to the pattern. `);
-    }, []),
-    connected: useCallback(({ outputs }) => {
-      pushLog(`Midi device connected! Available: ${outputs.map((o) => `"${o.name}"`).join(', ')}`);
-    }, []),
-    disconnected: useCallback(({ outputs }) => {
-      pushLog(`Midi device disconnected! Available: ${outputs.map((o) => `"${o.name}"`).join(', ')}`);
-    }, []),
-  });
 
   return (
     <div className="min-h-screen bg-[#2A3236] flex flex-col">
@@ -175,11 +79,7 @@ function App() {
               onChange={(_: any, __: any, value: any) => setCode(value)}
             />
             <span className="p-4 absolute top-0 right-0 text-xs whitespace-pre text-right">
-              {!cycle.started
-                ? `press ctrl+enter to play\n`
-                : code !== activeCode
-                ? `ctrl+enter to update\n`
-                : 'no changes\n'}
+              {!cycle.started ? `press ctrl+enter to play\n` : dirty ? `ctrl+enter to update\n` : 'no changes\n'}
             </span>
           </div>
           {error && (
@@ -188,16 +88,7 @@ function App() {
         </div>
         <button
           className="flex-none w-full border border-gray-700 p-2 bg-slate-700 hover:bg-slate-500"
-          onClick={() => {
-            // TODO: find out why sometimes, after a longer time coming back to the strudel repl, the button wont do anything
-            if (!cycle.started) {
-              // console.log('start');
-              activateCode();
-            } else {
-              // console.log('stop');
-              cycle.stop();
-            }
-          }}
+          onClick={() => togglePlay()}
         >
           {cycle.started ? 'pause' : 'play'}
         </button>
