92937fa72eadf178b5f938616613faef93d7574a
Felix Roos <roos@entrecode.de>
2022-03-12 21:58:17 +0100

    add lookaheadCycles to fix pianoroll

--- repl/src/pianoroll.mjs
+++ repl/src/pianoroll.mjs
@@ -12,19 +12,23 @@ Pattern.prototype.pianoroll = function ({
   const h = window.innerHeight;
   const midiRange = maxMidi - minMidi + 1;
   const height = h / midiRange;
-  this.draw((ctx, events, t) => {
-    ctx.fillStyle = background;
-    ctx.fillRect(0, 0, w, h);
-    events.forEach((event) => {
-      const isActive = event.whole.begin <= t && event.whole.end >= t;
-      ctx.fillStyle = isActive ? active : inactive;
-      const x = Math.round((event.whole.begin / timeframe) * w);
-      const width = Math.round(((event.whole.end - event.whole.begin) / timeframe) * w);
-      const y = Math.round(h - ((Number(event.value) - minMidi) / midiRange) * h);
-      const offset = (t / timeframe) * w;
-      const margin = 0;
-      ctx.fillRect(x - offset + margin + 1, y + 1, width - 2, height - 2);
-    });
-  }, timeframe * 2);
+  this.draw(
+    (ctx, events, t) => {
+      ctx.fillStyle = background;
+      ctx.fillRect(0, 0, w, h);
+      events.forEach((event) => {
+        const isActive = event.whole.begin <= t && event.whole.end >= t;
+        ctx.fillStyle = isActive ? active : inactive;
+        const x = Math.round((event.whole.begin / timeframe) * w);
+        const width = Math.round(((event.whole.end - event.whole.begin) / timeframe) * w);
+        const y = Math.round(h - ((Number(event.value) - minMidi) / midiRange) * h);
+        const offset = (t / timeframe) * w;
+        const margin = 0;
+        ctx.fillRect(x - offset + margin + 1, y + 1, width - 2, height - 2);
+      });
+    },
+    timeframe,
+    2 // lookaheadCycles
+  );
   return this;
 };
