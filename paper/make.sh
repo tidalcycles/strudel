e8f6825b64ac5f8ec83be06257dac6ecfdfcfe69
alex <alex@slab.org>
2022-03-22 15:56:26 +0000

    integrate with WAC latex template

--- paper/make.sh
+++ paper/make.sh
@@ -7,7 +7,7 @@ fi
 # --template=templates/template.latex \
 
 pandoc -s paper.md \
-  --from markdown+auto_identifiers -V colorlinks --number-sections --citeproc --pdf-engine=xelatex \
+  --from markdown+auto_identifiers --pdf-engine=xelatex --template latex-template.tex -V colorlinks --number-sections --citeproc --pdf-engine=xelatex \
   --dpi=300 --bibliography strudel.bib -o paper.pdf
 
 pandoc -s paper.md --filter filter.py --citeproc --bibliography strudel.bib \
