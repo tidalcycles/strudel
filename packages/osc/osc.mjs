0b8919de61d2e277b100a109b177ea03ef24d969
alex <alex@slab.org>
2022-03-29 08:12:34 +0100

    send actual osc parameters

--- packages/osc/osc.mjs
+++ packages/osc/osc.mjs
@@ -1,4 +1,3 @@
-
 import OSC from './node_modules/osc-js/lib/osc.js';
 import { Pattern, isPattern } from '@strudel.cycles/core/strudel.mjs';
 //import {OSC} from 'osc-js';
@@ -7,16 +6,12 @@ const comm = new OSC();
 comm.open();
 
 Pattern.prototype.osc = function () {
-    return this._withEvent((event) => {
-        const onTrigger = (time, event) => {
-            //const keyvals = Object.entries(event.value).flat();
-            //const message = new OSC.Message("/dirt/play", ...keyvals);   
-            const message = new OSC.Message(
-                '/dirt/play',
-                ...['_id_', '1', 'cps', 0.5625, 'cycle', 412.3333435058594, 'delta', 0.592592716217041, 'orbit', 0, 's', 'hh'],
-              );
-            comm.send(message);
-        };
-        return event.setContext({ ...event.context, onTrigger });
-    });
+  return this._withEvent((event) => {
+    const onTrigger = (time, event) => {
+      const keyvals = Object.entries(event.value).flat();
+      const message = new OSC.Message('/dirt/play', ...keyvals);
+      comm.send(message);
+    };
+    return event.setContext({ ...event.context, onTrigger });
+  });
 };
