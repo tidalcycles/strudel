723b239fda4cfae0d89df9fa7b9bfc3083545ab6
alex <alex@slab.org>
2022-04-01 23:15:39 +0100

    formatting

--- packages/osc/osc.mjs
+++ packages/osc/osc.mjs
@@ -12,7 +12,7 @@ Pattern.prototype.osc = function () {
     const onTrigger = (time, event) => {
       const keyvals = Object.entries(event.value).flat();
       const ts = Math.floor(startTime + ((time + latency) * 1000));
-      const message = new OSC.Message('/dirt/play',...keyvals);
+      const message = new OSC.Message('/dirt/play', ...keyvals);
       const bundle = new OSC.Bundle([message], ts);
       bundle.timestamp(ts); // workaround for https://github.com/adzialocha/osc-js/issues/60
       comm.send(bundle);
