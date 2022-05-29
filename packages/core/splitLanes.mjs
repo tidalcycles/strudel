a996a7043d82d808b2e4e30afd56d4eb1e02b52f
Felix Roos <flix91@gmail.com>
2022-05-29 09:58:04 +0200

    remove splitLanes from branch

--- packages/core/splitLanes.mjs
+++
@@ -1,8 +0,0 @@
-function splitLanes(haps) {
-  // TODO: split haps into multiple arrays so that no overlapping events are in the same array
-  // also make sure the lanes are packed as tight as possible
-  // use logic analogous to drawLine
-  return [haps];
-}
-
-export default splitLanes;
