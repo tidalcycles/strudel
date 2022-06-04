eb0adf4194821a5abee0a9e7ccb1587642171252
Felix Roos <flix91@gmail.com>
2022-06-04 23:54:39 +0200

    use evaluateOnly in MaxiRepl

--- talk/MaxiRepl.jsx
+++ talk/MaxiRepl.jsx
@@ -1,10 +1,10 @@
-import { Tone } from '@strudel.cycles/tone';
 import { evalScope } from '@strudel.cycles/eval';
 import { MiniRepl as _MiniRepl } from '@strudel.cycles/react';
 import controls from '@strudel.cycles/core/controls.mjs';
 import { loadWebDirt } from '@strudel.cycles/webdirt';
 import { materialPalenightLarge } from './materialPalenightThemeLarge';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { cleanupDraw, cleanupUi, Tone } from '@strudel.cycles/tone';
 
 export const defaultSynth = new Tone.PolySynth().chain(new Tone.Gain(0.5), Tone.Destination).set({
   oscillator: { type: 'triangle' },
@@ -13,7 +13,7 @@ export const defaultSynth = new Tone.PolySynth().chain(new Tone.Gain(0.5), Tone.
   },
 });
 
-evalScope(
+const init = evalScope(
   Tone,
   controls,
   import('@strudel.cycles/core'),
@@ -35,6 +35,14 @@ loadWebDirt({
 export function MaxiRepl({ code, canvasHeight = 500 }) {
   const [exampleIndex, setExampleIndex] = useState(0);
   const examples = Array.isArray(code) ? code : [code];
+  const [ready, setReady] = useState(false);
+  useEffect(() => {
+    init.then(() => setReady(true));
+  }, []);
+  useEffect(() => {
+    cleanupDraw();
+    cleanupUi();
+  }, [exampleIndex]);
   return (
     <div className="text-left block relative">
       {examples.length > 1 && (
@@ -55,6 +63,7 @@ export function MaxiRepl({ code, canvasHeight = 500 }) {
           defaultSynth={defaultSynth}
           hideOutsideView={true}
           theme={materialPalenightLarge}
+          init={ready}
         />
       </div>
       <canvas
