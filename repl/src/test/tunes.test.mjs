235e82a348c76c6098496e06af15a713051572b4
Felix Roos <flix91@gmail.com>
2022-08-09 23:28:36 +0200

    make tests a little bit faster

--- repl/src/test/tunes.test.mjs
+++ repl/src/test/tunes.test.mjs
@@ -1,17 +1,17 @@
 import { queryCode, testCycles } from '../runtime.mjs';
 import * as snaps from '../tunes.snapshot.mjs';
 import * as tunes from '../tunes.mjs';
-import { describe, it, expect } from 'vitest';
+import { describe, it } from 'vitest';
 
 const tuneKeys = Object.keys(tunes);
 
-async function testTune(key) {
+async function testTune(key, expect) {
   const haps = await queryCode(tunes[key], testCycles[key] || 1);
   expect(haps).toEqual(snaps[key]);
 }
 
 describe.concurrent('renders tunes', () => {
-  tuneKeys.slice(0, 40).forEach((key) => {
-    it(`tune: ${key}`, async ({ expect }) => await testTune(key));
+  tuneKeys.forEach((key) => {
+    it(`tune: ${key}`, async ({ expect }) => await testTune(key, expect));
   });
 });
