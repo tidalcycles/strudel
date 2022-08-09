f955d77d71a8455b339a044321b733102b4dadbf
Felix Roos <flix91@gmail.com>
2022-08-10 00:17:58 +0200

    use vitest snapshot feature for tunes

--- repl/src/test/tunes.test.mjs
+++ repl/src/test/tunes.test.mjs
@@ -1,17 +1,14 @@
 import { queryCode, testCycles } from '../runtime.mjs';
-import * as snaps from '../tunes.snapshot.mjs';
 import * as tunes from '../tunes.mjs';
 import { describe, it } from 'vitest';
 
 const tuneKeys = Object.keys(tunes);
 
-async function testTune(key, expect) {
-  const haps = await queryCode(tunes[key], testCycles[key] || 1);
-  expect(haps).toEqual(snaps[key]);
-}
-
-describe.concurrent('renders tunes', () => {
+describe('renders tunes', () => {
   tuneKeys.forEach((key) => {
-    it(`tune: ${key}`, async ({ expect }) => await testTune(key, expect));
+    it(`tune: ${key}`, async ({ expect }) => {
+      const haps = await queryCode(tunes[key], testCycles[key] || 1);
+      expect(haps).toMatchSnapshot();
+    });
   });
 });
