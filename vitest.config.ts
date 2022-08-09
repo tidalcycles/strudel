17d0b7d291daf9cd416625205e90f48739a3a048
Felix Roos <flix91@gmail.com>
2022-08-09 23:09:02 +0200

    bump vitest + remove mocha

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { configDefaults, defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    exclude: [...configDefaults.exclude, 'repl/src/test/*'],
-    environment: 'happy-dom', // or 'jsdom', 'node' // https://vitest.dev/guide/features.html#chai-and-jest-expect-compatibility
-    // environment: 'happy-dom', // or 'jsdom', 'node' // https://vitest.dev/guide/features.html#chai-and-jest-expect-compatibility
-  },
-});
