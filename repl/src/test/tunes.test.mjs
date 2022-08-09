22c60fb8947d6ce087a251591819e4fd8ce20db3
Felix Roos <flix91@gmail.com>
2022-08-09 23:08:01 +0200

    move tunes test to vitest

--- repl/src/test/tunes.test.mjs
+++ repl/src/test/tunes.test.mjs
@@ -1,19 +1,17 @@
 import { queryCode, testCycles } from '../runtime.mjs';
 import * as snaps from '../tunes.snapshot.mjs';
 import * as tunes from '../tunes.mjs';
-import { strict as assert } from 'assert';
-// import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from 'vitest';
+
+const tuneKeys = Object.keys(tunes);
+
 async function testTune(key) {
-  // console.log('test tune', key);
   const haps = await queryCode(tunes[key], testCycles[key] || 1);
-  assert.deepStrictEqual(haps, snaps[key]);
-  // expect(haps).toEqual(snaps[key]);
+  expect(haps).toEqual(snaps[key]);
 }
 
-describe('renders tunes', () => {
-  Object.keys(tunes).forEach((key) => {
-    it(`tune: ${key}`, async () => {
-      await testTune(key);
-    });
+describe.concurrent('renders tunes', () => {
+  tuneKeys.slice(0, 40).forEach((key) => {
+    it(`tune: ${key}`, async ({ expect }) => await testTune(key));
   });
 });
