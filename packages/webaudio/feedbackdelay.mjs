d3645f6c63e1ce1ededd6ca6a911e52b3472ed64
Felix Roos <flix91@gmail.com>
2022-09-18 22:42:30 +0200

    fix tests

--- packages/webaudio/feedbackdelay.mjs
+++ packages/webaudio/feedbackdelay.mjs
@@ -1,28 +1,30 @@
-class FeedbackDelayNode extends DelayNode {
-  constructor(ac, wet, time, feedback) {
-    super(ac);
-    wet = Math.abs(wet);
-    this.delayTime.value = time;
+if (typeof DelayNode !== 'undefined') {
+  class FeedbackDelayNode extends DelayNode {
+    constructor(ac, wet, time, feedback) {
+      super(ac);
+      wet = Math.abs(wet);
+      this.delayTime.value = time;
 
-    const feedbackGain = ac.createGain();
-    feedbackGain.gain.value = Math.min(Math.abs(feedback), 0.995);
+      const feedbackGain = ac.createGain();
+      feedbackGain.gain.value = Math.min(Math.abs(feedback), 0.995);
 
-    const delayGain = ac.createGain();
-    delayGain.gain.value = wet;
-    this.delayGain = delayGain;
+      const delayGain = ac.createGain();
+      delayGain.gain.value = wet;
+      this.delayGain = delayGain;
 
-    this.connect(feedbackGain);
-    this.connect(delayGain);
-    feedbackGain.connect(this);
+      this.connect(feedbackGain);
+      this.connect(delayGain);
+      feedbackGain.connect(this);
 
-    this.connect = (target) => delayGain.connect(target);
-    return this;
+      this.connect = (target) => delayGain.connect(target);
+      return this;
+    }
+    start(t) {
+      this.delayGain.gain.setValueAtTime(this.delayGain.gain.value, t + this.delayTime.value);
+    }
   }
-  start(t) {
-    this.delayGain.gain.setValueAtTime(this.delayGain.gain.value, t + this.delayTime.value);
-  }
+
+  AudioContext.prototype.createFeedbackDelay = function (wet, time, feedback) {
+    return new FeedbackDelayNode(this, wet, time, feedback);
+  };
 }
-
-AudioContext.prototype.createFeedbackDelay = function (wet, time, feedback) {
-  return new FeedbackDelayNode(this, wet, time, feedback);
-};
