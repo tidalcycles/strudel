2168dbc6fc382409cb87ce9612ef988986cd9094
Felix Roos <flix91@gmail.com>
2022-11-01 17:09:18 +0100

    flat rave

--- repl/src/tunes.mjs
+++ repl/src/tunes.mjs
@@ -929,9 +929,23 @@ export const holyflute = `"c3 eb3(3,8) c4/2 g3*2"
 .pianoroll({fold:0,autorange:0,vertical:0,cycles:12,smear:0,minMidi:40})
 `;
 
-export const drums909 = `stack(
-  s("bd:2*2").end(.2),
-  s("~@7 <~@3 bd:2>").end(.2),
-  s("hh:4*4").sometimes(fast("2")).speed(1.3).room(.5),
-  s("~ cp:2").room("<0 1>").delay("<0 .5>").delaytime(.125)
-).gain(.5).bank('RolandTR909')`;
+export const flatrave = `stack(
+  s("bd*2,~ [cp,sd]").bank('RolandTR909'),
+  
+  s("hh:1*4").sometimes(fast("2"))
+  .rarely(x=>x.speed(".5").delay(.5))
+  .end(perlin.range(0.02,.05).slow(8))
+  .bank('RolandTR909').room(.5)
+  .gain("0.4,0.4(5,8)"),
+  
+  note("<0 2 5 3>".scale('G1 minor')).struct("x(5,8)")
+  .s('sawtooth').decay(.1).sustain(0),
+  
+  note("<G4 A4 Bb4 A4>,Bb3,D3").struct("~ x*2").s('square').clip(1)
+  .cutoff(sine.range(500,4000).slow(16)).resonance(10)
+  .decay(sine.slow(15).range(.05,.2)).sustain(0)
+  .room(.5).gain(.3).delay(.2).mask("<0 1@3>/8"),
+  
+  "0 5 3 2".sometimes(slow(2)).off(1/8,add(5)).scale('G4 minor').note()
+  .decay(.05).sustain(0).delay(.2).degradeBy(.5).mask("<0 1>/16")
+)`;
