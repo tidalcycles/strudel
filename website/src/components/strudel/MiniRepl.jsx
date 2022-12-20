b8f991eddbbbf6f21ba5716b8d067bade8868078
Felix Roos <flix91@gmail.com>
2022-12-20 20:42:42 +0100

    simplify writing mdx files

--- website/src/components/strudel/MiniRepl.jsx
+++ website/src/components/strudel/MiniRepl.jsx
@@ -1,25 +1,35 @@
 import { evalScope, controls } from '@strudel.cycles/core';
-import { MiniRepl as _MiniRepl } from '@strudel.cycles/react';
 import { samples } from '@strudel.cycles/webaudio';
+import { useEffect, useState } from 'react';
 
-fetch('https://strudel.tidalcycles.org/EmuSP12.json')
-  .then((res) => res.json())
-  .then((json) => samples(json, 'https://strudel.tidalcycles.org/EmuSP12/'));
+if (typeof window !== 'undefined') {
+  fetch('https://strudel.tidalcycles.org/EmuSP12.json')
+    .then((res) => res.json())
+    .then((json) => samples(json, 'https://strudel.tidalcycles.org/EmuSP12/'));
 
-evalScope(
-  controls,
-  import('@strudel.cycles/core'),
-  // import('@strudel.cycles/tone'),
-  import('@strudel.cycles/tonal'),
-  import('@strudel.cycles/mini'),
-  import('@strudel.cycles/midi'),
-  import('@strudel.cycles/xen'),
-  import('@strudel.cycles/webaudio'),
-  import('@strudel.cycles/osc'),
-);
+  evalScope(
+    controls,
+    import('@strudel.cycles/core'),
+    // import('@strudel.cycles/tone'),
+    import('@strudel.cycles/tonal'),
+    import('@strudel.cycles/mini'),
+    import('@strudel.cycles/midi'),
+    import('@strudel.cycles/xen'),
+    import('@strudel.cycles/webaudio'),
+    import('@strudel.cycles/osc'),
+  );
+}
 
 // prebake();
 
 export function MiniRepl({ tune }) {
-  return <_MiniRepl tune={tune} hideOutsideView={true} />;
+  const [Repl, setRepl] = useState();
+  useEffect(() => {
+    // we have to load this package on the client
+    // because codemirror throws an error on the server
+    import('@strudel.cycles/react').then((res) => {
+      setRepl(() => res.MiniRepl);
+    });
+  }, []);
+  return Repl ? <Repl tune={tune} hideOutsideView={true} /> : <pre>{tune}</pre>;
 }
