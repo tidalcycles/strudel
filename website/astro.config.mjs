d0013f55a25b30933d0bf9abddb4bb63dd81633d
Felix Roos <flix91@gmail.com>
2022-12-20 19:50:32 +0100

    format

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -8,14 +8,17 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-  // Enable Preact to support Preact JSX components.
-  preact(),
-  // Enable React for the Algolia search component.
-  react(), mdx(), tailwind()],
-  site: `https://strudel.tidalcycles.org`
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    // Enable React for the Algolia search component.
+    react(),
+    mdx(),
+    tailwind(),
+  ],
+  site: `https://strudel.tidalcycles.org`,
 });
