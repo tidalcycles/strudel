b1202fcd5528a6954a0c402cd441466eea735a1b
Felix Roos <flix91@gmail.com>
2022-12-20 21:38:08 +0100

    split tutorial into multiple pages + improve layout a bit

--- website/src/config.ts
+++ website/src/config.ts
@@ -29,9 +29,9 @@ export const KNOWN_LANGUAGES = {
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
+export const GITHUB_EDIT_URL = `https://github.com/tidalcycles/strudel/tree/main/website`;
 
-export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
+export const COMMUNITY_INVITE_URL = `https://discord.com/invite/HGEdXmRkzT`;
 
 // See "Algolia" section of the README for more information.
 export const ALGOLIA = {
@@ -43,7 +43,14 @@ export const ALGOLIA = {
 export type Sidebar = Record<typeof KNOWN_LANGUAGE_CODES[number], Record<string, { text: string; link: string }[]>>;
 export const SIDEBAR: Sidebar = {
   en: {
-    Learn: [{ text: 'Tutorial', link: 'tutorial' }],
-    // 'Another Section': [{ text: 'Page 4', link: 'en/page-4' }],
+    Learn: [
+      { text: 'Getting Started', link: 'learn/getting-started' },
+      { text: 'Mini Notation', link: 'learn/mini-notation' },
+      { text: 'Synths, Samples & FX', link: 'learn/synths-samples-effects' },
+      { text: 'Functions', link: 'learn/functions' },
+      { text: 'Signals', link: 'learn/signals' },
+      { text: 'Tonal', link: 'learn/tonal' },
+      { text: 'Outputs', link: 'learn/outputs' },
+    ],
   },
 };
