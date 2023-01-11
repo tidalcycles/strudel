83ff6a450fbec8870890c4bd4e7c07c3114ea468
gogins <michael.gogins@gmail.com>
2023-01-11 13:58:46 -0500

    Fixed problem with undici fetch.

--- packages/webaudio/sampler.mjs
+++ packages/webaudio/sampler.mjs
@@ -126,7 +126,7 @@ export const samples = async (sampleMap, baseUrl = sampleMap._base || '') => {
     if (sampleMap.startsWith('github:')) {
       const [_, path] = sampleMap.split('github:');
       sampleMap = `https://raw.githubusercontent.com/${path}/strudel.json`;
-      // undici fails if a pathspace has more than one directory separator.
+      // undici fails if a patchspec has a duplicated directory separator.
       sampleMap = sampleMap.replace("//strudel.json", "/strudel.json");
     }
     if (typeof fetch !== 'function') {
