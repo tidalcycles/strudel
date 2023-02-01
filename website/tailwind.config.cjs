6f5f8cc7d8b6043e66d9a8680054edffff653701
Felix Roos <flix91@gmail.com>
2023-02-01 22:30:24 +0100

    fix: minirepl styles

--- website/tailwind.config.cjs
+++ website/tailwind.config.cjs
@@ -3,7 +3,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    '../packages/react/src/**/*.{html,js,jsx,md,mdx,ts,tsx}',
+  ],
   theme: {
     extend: {
       colors: {
