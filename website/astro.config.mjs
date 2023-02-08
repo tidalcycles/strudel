b6e42876ad94262445b6c3f810462247caa6a4bb
Felix Roos <flix91@gmail.com>
2023-02-08 20:32:03 +0100

    fix cache rule + disable service worker in dev

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -37,16 +37,15 @@ export default defineConfig({
         globPatterns: ['**/*.{js,css,html,ico,png,svg,json,wav,mp3,ogg}'],
         runtimeCaching: [
           {
-            urlPattern: ({ url }) => {
+            urlPattern: ({ url }) =>
               [
                 /^https:\/\/raw\.githubusercontent\.com\/.*/i,
                 /^https:\/\/freesound\.org\/.*/i,
                 /^https:\/\/shabda\.ndre\.gr\/.*/i,
-              ].some((regex) => regex.test(url));
-            },
+              ].some((regex) => regex.test(url)),
             handler: 'CacheFirst',
             options: {
-              cacheName: 'github-files',
+              cacheName: 'external-samples',
               expiration: {
                 maxEntries: 5000,
                 maxAgeSeconds: 60 * 60 * 24 * 30, // <== 14 days
