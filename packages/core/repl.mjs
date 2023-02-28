1f7e2932048f8579f12b117294a39c15b87758dd
Felix Roos <flix91@gmail.com>
2023-02-28 23:56:11 +0100

    add setcps to global scope

--- packages/core/repl.mjs
+++ packages/core/repl.mjs
@@ -2,6 +2,7 @@ import { Cyclist } from './cyclist.mjs';
 import { evaluate as _evaluate } from './evaluate.mjs';
 import { logger } from './logger.mjs';
 import { setTime } from './time.mjs';
+import { evalScope } from './evaluate.mjs';
 
 export function repl({
   interval,
@@ -17,13 +18,12 @@ export function repl({
 }) {
   const scheduler = new Cyclist({
     interval,
-    onTrigger: async (hap, deadline, duration) => {
+    onTrigger: async (hap, deadline, duration, cps) => {
       try {
         if (!hap.context.onTrigger || !hap.context.dominantTrigger) {
           await defaultOutput(hap, deadline, duration);
         }
         if (hap.context.onTrigger) {
-          const cps = 1;
           // call signature of output / onTrigger is different...
           await hap.context.onTrigger(getTime() + deadline, hap, getTime(), cps);
         }
@@ -58,5 +58,10 @@ export function repl({
   const stop = () => scheduler.stop();
   const start = () => scheduler.start();
   const pause = () => scheduler.pause();
-  return { scheduler, evaluate, start, stop, pause };
+  const setCps = (cps) => scheduler.setCps(cps);
+  evalScope({
+    setCps,
+    setcps: setCps,
+  });
+  return { scheduler, evaluate, start, stop, pause, setCps };
 }
