e72fdc1341c948e670314f6962cc99e92f2a54d2
alex <alex@slab.org>
2023-03-01 23:55:57 +0000

    reinstate slice/splice, fixing up "unit" param using new global cps

--- packages/webaudio/webaudio.mjs
+++ packages/webaudio/webaudio.mjs
@@ -191,7 +191,7 @@ function effectSend(input, effect, wet) {
 }
 
 // export const webaudioOutput = async (t, hap, ct, cps) => {
-export const webaudioOutput = async (hap, deadline, hapDuration) => {
+export const webaudioOutput = async (hap, deadline, hapDuration, cps) => {
   const ac = getAudioContext();
   hap.ensureObjectValue();
 
@@ -311,8 +311,9 @@ export const webaudioOutput = async (hap, deadline, hapDuration) => {
     }
     bufferSource.playbackRate.value = Math.abs(speed) * bufferSource.playbackRate.value;
     if (unit === 'c') {
+      console.log(cps)
       // are there other units?
-      bufferSource.playbackRate.value = bufferSource.playbackRate.value * bufferSource.buffer.duration;
+      bufferSource.playbackRate.value = bufferSource.playbackRate.value * bufferSource.buffer.duration * cps;
     }
     let duration = soundfont || clip ? hapDuration : bufferSource.buffer.duration / bufferSource.playbackRate.value;
     // "The computation of the offset into the sound is performed using the sound buffer's natural sample rate,
@@ -385,7 +386,7 @@ export const webaudioOutput = async (hap, deadline, hapDuration) => {
   chain[0].onended = () => chain.concat([delaySend, reverbSend]).forEach((n) => n?.disconnect());
 };
 
-export const webaudioOutputTrigger = (t, hap, ct, cps) => webaudioOutput(hap, t - ct, hap.duration / cps);
+export const webaudioOutputTrigger = (t, hap, ct, cps) => webaudioOutput(hap, t - ct, hap.duration / cps, cps);
 
 Pattern.prototype.webaudio = function () {
   // TODO: refactor (t, hap, ct, cps) to (hap, deadline, duration) ?
