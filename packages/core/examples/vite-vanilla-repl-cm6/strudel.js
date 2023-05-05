d96f3f9dd4f9a53c9847a31876570133b2b880f4
Felix Roos <flix91@gmail.com>
2023-05-05 11:09:44 +0200

    vanilla-repl-cm6: style + codesplitting

--- packages/core/examples/vite-vanilla-repl-cm6/strudel.js
+++ packages/core/examples/vite-vanilla-repl-cm6/strudel.js
@@ -1,38 +1,29 @@
-import { controls, repl, evalScope } from "@strudel.cycles/core";
-import { transpiler } from "@strudel.cycles/transpiler";
-import {
-  getAudioContext,
-  webaudioOutput,
-  initAudioOnFirstClick,
-  registerSynthSounds
-} from "@strudel.cycles/webaudio";
-import { registerSoundfonts } from "@strudel.cycles/soundfonts";
+import { getAudioContext, webaudioOutput, initAudioOnFirstClick, registerSynthSounds } from '@strudel.cycles/webaudio';
 
 const initAudio = initAudioOnFirstClick();
 const ctx = getAudioContext();
 
-const loadModules = (scope = {}) =>
-  evalScope(
+export async function initStrudel(options = {}) {
+  const [{ controls, repl, evalScope }, { registerSoundfonts }, { transpiler }] = await Promise.all([
+    import('@strudel.cycles/core'),
+    import('@strudel.cycles/soundfonts'),
+    import('@strudel.cycles/transpiler'),
+  ]);
+
+  const loadModules = evalScope(
     controls,
-    import("@strudel.cycles/core"),
-    import("@strudel.cycles/mini"),
-    import("@strudel.cycles/tonal"),
-    import("@strudel.cycles/webaudio"),
-    scope
+    import('@strudel.cycles/core'),
+    import('@strudel.cycles/mini'),
+    import('@strudel.cycles/tonal'),
+    import('@strudel.cycles/webaudio'),
   );
 
-export async function initStrudel(options = {}) {
-  await Promise.all([
-    initAudio,
-    loadModules(),
-    registerSynthSounds(),
-    registerSoundfonts()
-  ]);
+  await Promise.all([initAudio, loadModules, registerSynthSounds(), registerSoundfonts()]);
 
   return repl({
     defaultOutput: webaudioOutput,
     getTime: () => ctx.currentTime,
     transpiler,
-    ...options
+    ...options,
   });
 }
