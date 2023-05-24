b63f4eb503db9edd4182d5895a33677b5440dc84
Daria Cotocu <daria@MacBook-Pro-62.arts.local>
2023-05-24 18:58:11 +0100

    Update solmization.test.js

--- packages/core/test/solmization.test.js
+++ packages/core/test/solmization.test.js
@@ -15,30 +15,26 @@ test('midi2note - solfeggio', () => {
   assert.equal(result, expected);
 });
 
-
 test('midi2note - indian', () => {
-    const result = midi2note(60, 'indian');
-    const expected = 'Sa4';
-    assert.equal(result, expected);
-  });
+  const result = midi2note(60, 'indian');
+  const expected = 'Sa4';
+  assert.equal(result, expected);
+});
 
 test('midi2note - german', () => {
-    const result = midi2note(60, 'german');
-    const expected = 'C4';
-    assert.equal(result, expected);
-  });
+  const result = midi2note(60, 'german');
+  const expected = 'C4';
+  assert.equal(result, expected);
+});
 
 test('midi2note - byzantine', () => {
-    const result = midi2note(60, 'byzantine');
-    const expected = 'Ni4';
-    assert.equal(result, expected);
-  });
+  const result = midi2note(60, 'byzantine');
+  const expected = 'Ni4';
+  assert.equal(result, expected);
+});
 
 test('midi2note - japanese', () => {
-    const result = midi2note(60, 'japanese');
-    const expected = 'I4';
-    assert.equal(result, expected);
-  });
-
-
-
+  const result = midi2note(60, 'japanese');
+  const expected = 'I4';
+  assert.equal(result, expected);
+});
