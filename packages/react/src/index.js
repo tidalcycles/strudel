5850106eefce4b4917a4c3ca72393b077429b7a2
Felix Roos <flix91@gmail.com>
2023-11-02 22:33:16 +0100

    restore react hooks

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -6,6 +6,8 @@ export { default as useHighlighting } from './hooks/useHighlighting'; // !SSR
 export { default as useStrudel } from './hooks/useStrudel'; // !SSR
 export { default as usePostMessage } from './hooks/usePostMessage';
 export { default as usePatternFrame } from './hooks/usePatternFrame';
+export { default as useKeydown } from './hooks/useKeydown';
+export { useWidgets } from './hooks/useWidgets';
 export { default as useEvent } from './hooks/useEvent';
 export { default as strudelTheme } from './themes/strudel-theme';
 export { default as teletext } from './themes/teletext';
