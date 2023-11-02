9ebb7edde9c32a41fce54c5807bcd14205c312f0
Felix Roos <flix91@gmail.com>
2023-11-02 11:29:27 +0100

    remove old export

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -5,7 +5,6 @@ export * from './components/MiniRepl'; // !SSR
 export { default as useHighlighting } from './hooks/useHighlighting'; // !SSR
 export { default as useStrudel } from './hooks/useStrudel'; // !SSR
 export { default as usePostMessage } from './hooks/usePostMessage';
-export { default as useKeydown } from './hooks/useKeydown';
 export { default as useEvent } from './hooks/useEvent';
 export { default as strudelTheme } from './themes/strudel-theme';
 export { default as teletext } from './themes/teletext';
