0a280e939927ad4956ce14b3e42139935cdf83be
Ámbar Tenorio Fornés <ambar@atenor.io>
2023-11-22 15:47:16 +0100

    prettier code formatting

--- packages/hydra/hydra.mjs
+++ packages/hydra/hydra.mjs
@@ -1,14 +1,13 @@
 import { getDrawContext } from '@strudel.cycles/core';
 
-let options = ""
+let options = '';
 
 export async function initHydra(config) {
-
   //load and init hydra
   if (!document.getElementById('hydra-canvas')) {
     const { canvas: testCanvas } = getDrawContext();
     await import('https://unpkg.com/hydra-synth');
-    h = new Hydra({detectAudio: config?.audio });
+    h = new Hydra({ detectAudio: config?.audio });
     h.canvas.id = 'hydra-canvas';
     h.canvas.style.position = 'absolute';
     h.canvas.style.top = '0px';
@@ -19,11 +18,10 @@ export async function initHydra(config) {
   if (options != JSON.stringify(config)) {
     options = JSON.stringify(config);
 
-    new Hydra(
-      {
-        canvas: document.getElementById('hydra-canvas'),
-        detectAudio: config?.audio
-      })
+    new Hydra({
+      canvas: document.getElementById('hydra-canvas'),
+      detectAudio: config?.audio,
+    });
   }
 }
 
