020c3264587cdf525696851e7ad11061e5260a99
Alexandre G.-Raymond <alex@ndre.gr>
2023-11-26 22:52:17 +0100

    Codeformat for search

--- website/src/components/Header/Search.tsx
+++ website/src/components/Header/Search.tsx
@@ -43,7 +43,12 @@ export default function Search() {
 
   return (
     <>
-      <button type="button" ref={searchButtonRef} onClick={onOpen} className="rounded-md bg-slate-900 w-full px-2 search-button">
+      <button
+        type="button"
+        ref={searchButtonRef}
+        onClick={onOpen}
+        className="rounded-md bg-slate-900 w-full px-2 search-button"
+      >
         <svg width="24" height="24" fill="none">
           <path
             d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
