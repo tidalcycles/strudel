99194814a98126ba1347c01496972be744afeaf6
Felix Roos <flix91@gmail.com>
2023-12-14 10:36:56 +0100

    move code hashing helpers to core

--- packages/core/util.mjs
+++ packages/core/util.mjs
@@ -274,3 +274,31 @@ export const sol2note = (n, notation = 'letters') => {
   const oct = Math.floor(n / 12) - 1;
   return note + oct;
 };
+
+// code hashing helpers
+
+export function unicodeToBase64(text) {
+  const utf8Bytes = new TextEncoder().encode(text);
+  const base64String = btoa(String.fromCharCode(...utf8Bytes));
+  return base64String;
+}
+
+export function base64ToUnicode(base64String) {
+  const utf8Bytes = new Uint8Array(
+    atob(base64String)
+      .split('')
+      .map((char) => char.charCodeAt(0)),
+  );
+  const decodedText = new TextDecoder().decode(utf8Bytes);
+  return decodedText;
+}
+
+export function code2hash(code) {
+  return encodeURIComponent(unicodeToBase64(code));
+  //return '#' + encodeURIComponent(btoa(code));
+}
+
+export function hash2code(hash) {
+  return base64ToUnicode(decodeURIComponent(hash));
+  //return atob(decodeURIComponent(codeParam || ''));
+}
