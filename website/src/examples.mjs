f85fb35c669bc52ad8fa79d10e4a38157242042e
Felix Roos <flix91@gmail.com>
2024-01-18 13:46:35 +0100

    minirepl multi tune support + add other getting-started examples (more to come)

---
+++ website/src/examples.mjs
@@ -0,0 +1,81 @@
+export const examples = [
+  `// "coastline" @by eddyflux
+await samples('github:eddyflux/crate')
+setcps(.75)
+let chords = chord("<Bbm9 Fm9>/4").dict('ireal')
+stack(
+  stack( // DRUMS
+    s("bd").struct("<[x*<1 2> [~@3 x]] x>"),
+    s("~ [rim, sd:<2 3>]").room("<0 .2>"),
+    n("[0 <1 3>]*<2!3 4>").s("hh"),
+    s("rd:<1!3 2>*2").mask("<0 0 1 1>/16").gain(.5)
+  ).bank('crate')
+  .mask("<[0 1] 1 1 1>/16".early(.5))
+  , // CHORDS
+  chords.offset(-1).voicing().s("gm_epiano1:1")
+  .phaser(4).room(.5)
+  , // MELODY
+  n("<0!3 1*2>").set(chords).mode("root:g2")
+  .voicing().s("gm_acoustic_bass"),
+  chords.n("[0 <4 3 <2 5>>*2](<3 5>,8)")
+  .set(x).anchor("D5").voicing()
+  .segment(4).clip(rand.range(.4,.8))
+  .room(.75).shape(.3).delay(.25)
+  .fm(sine.range(3,8).slow(8))
+  .lpf(sine.range(500,1000).slow(8)).lpq(5)
+  .rarely(ply("2")).chunk(4, fast(2))
+  .gain(perlin.range(.6, .9))
+  .mask("<0 1 1 0>/16")
+)
+.late("[0 .01]*4").late("[0 .01]*2").size(4)`,
+  `// "broken cut 1" @by froos
+
+await samples('github:tidalcycles/Dirt-Samples/master')
+samples({
+  'slap': 'https://cdn.freesound.org/previews/495/495416_10350281-lq.mp3',
+  'whirl': 'https://cdn.freesound.org/previews/495/495313_10350281-lq.mp3',
+  'attack': 'https://cdn.freesound.org/previews/494/494947_10350281-lq.mp3'
+})
+
+setcps(1.25)
+
+note("[c2 ~](3,8)*2,eb,g,bb,d").s("sawtooth")
+  .noise(0.3)
+  .lpf(perlin.range(800,2000).mul(0.6))
+  .lpenv(perlin.range(1,5)).lpa(.25).lpd(.1).lps(0)
+  .add.mix(note("<0!3 [1 <4!3 12>]>")).late(.5)
+  .vib("4:.2")
+  .room(1).roomsize(4).slow(4)
+  .stack(
+    s("bd").late("<0.01 .251>"),
+    s("breaks165:1/2").fit()
+    .chop(4).sometimesBy(.4, ply("2"))
+    .sometimesBy(.1, ply("4")).release(.01)
+    .gain(1.5).sometimes(mul(speed("1.05"))).cut(1)
+    ,
+    s("<whirl attack>?").delay(".8:.1:.8").room(2).slow(8).cut(2),
+  ).reset("<x@30 [x*[8 [8 [16 32]]]]@2>".late(2))`,
+  `// "acidic tooth" @by eddyflux
+  setcps(1)
+  stack(
+    note("[<g1 f1>/8](<3 5>,8)")
+    .clip(perlin.range(.15,1.5))
+    .release(.1)
+    .s("sawtooth")
+    .lpf(sine.range(400,800).slow(16))
+    .lpq(cosine.range(6,14).slow(3))
+    .lpenv(sine.mul(4).slow(4))
+    .lpd(.2).lpa(.02)
+    .ftype('24db')
+    .rarely(add(note(12)))
+    .room(.2).shape(.3).postgain(.5)
+    .superimpose(x=>x.add(note(12)).delay(.5).bpf(1000))
+    .gain("[.2 1@3]*2") // fake sidechain
+    ,
+    stack(
+      s("bd*2").mask("<0@4 1@16>"),
+      s("hh*8").gain(saw.mul(saw.fast(2))).clip(sine)
+      .mask("<0@8 1@16>")
+    ).bank('RolandTR909')
+  )`,
+];
