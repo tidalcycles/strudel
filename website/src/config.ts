4f7415b0ec20c319bfc29f12eaa3cb067717d43e
Felix Roos <flix91@gmail.com>
2024-01-18 21:31:21 +0100

    rename

--- website/src/config.ts
+++ website/src/config.ts
@@ -58,7 +58,7 @@ export const SIDEBAR: Sidebar = {
       { text: 'What is Strudel?', link: 'workshop/getting-started' },
       { text: 'Showcase', link: 'intro/showcase' },
       { text: 'Blog', link: 'blog' },
-      { text: 'Community Oven', link: 'oven' },
+      { text: 'Community Bakery', link: 'oven' },
     ],
     Workshop: [
       // { text: 'Getting Started', link: 'workshop/getting-started' },
