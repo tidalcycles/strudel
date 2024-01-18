0778b4809e104b99a377d283123325dcdec13987
Felix Roos <flix91@gmail.com>
2024-01-18 13:47:34 +0100

    format

--- website/src/docs/Icon.jsx
+++ website/src/docs/Icon.jsx
@@ -2,7 +2,14 @@ export function Icon({ type }) {
   if (type === 'skip') {
     // !Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.
     return (
-      <svg fillRule="evenodd"  fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="16" width="10" viewBox="0 0 320 512">
+      <svg
+        fillRule="evenodd"
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+        height="16"
+        width="10"
+        viewBox="0 0 320 512"
+      >
         <path d="M52.5 440.6c-9.5 7.9-22.8 9.7-34.1 4.4S0 428.4 0 416V96C0 83.6 7.2 72.3 18.4 67s24.5-3.6 34.1 4.4l192 160L256 241V96c0-17.7 14.3-32 32-32s32 14.3 32 32V416c0 17.7-14.3 32-32 32s-32-14.3-32-32V271l-11.5 9.6-192 160z" />
       </svg>
     );
