13cf5ac51e110fb2204c7f07412b79b792c23c43
Felix Roos <flix91@gmail.com>
2024-01-21 19:17:51 +0100

    fix: errors for signals

--- packages/codemirror/highlight.mjs
+++ packages/codemirror/highlight.mjs
@@ -57,6 +57,9 @@ const visibleMiniLocations = StateField.define({
         // this is why we need to find a way to update the existing decorations, showing the ones that have an active range
         const haps = new Map();
         for (let hap of e.value.haps) {
+          if (!hap.context?.locations || !hap.whole) {
+            continue;
+          }
           for (let { start, end } of hap.context.locations) {
             let id = `${start}:${end}`;
             if (!haps.has(id) || haps.get(id).whole.begin.lt(hap.whole.begin)) {
@@ -64,7 +67,6 @@ const visibleMiniLocations = StateField.define({
             }
           }
         }
-
         visible = { atTime: e.value.atTime, haps };
       }
     }
