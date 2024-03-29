1af55c0d192211af6b7833c747142fec4a1310f7
Felix Roos <flix91@gmail.com>
2024-03-29 17:13:01 +0100

    add font file types to offline cache

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -69,7 +69,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       injectRegister: 'auto',
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,wav,mp3,ogg}'],
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,wav,mp3,ogg,ttf,woff2,TTF,otf}'],
         runtimeCaching: [
           {
             urlPattern: ({ url }) =>
