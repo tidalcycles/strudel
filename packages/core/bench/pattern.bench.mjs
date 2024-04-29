fd1a1c3a1b8be7543a9082dc2e8e9eb76ed3dbc6
Alex McLean <alex@slab.org>
2024-04-29 12:04:36 +0100

    more tactus benchmarks

--- packages/core/bench/pattern.bench.mjs
+++ packages/core/bench/pattern.bench.mjs
@@ -1,13 +1,45 @@
 import { describe, bench } from 'vitest';
 
-import { sequence } from '../index.mjs';
+import { calculateTactus, sequence, stack } from '../index.mjs';
 
 const pat64 = sequence(...Array(64).keys());
 
-bench(
-  'iter',
-  () => {
-    pat64.iter(64).fast(64).firstCycle();
-  },
-  { time: 1000 },
-);
+describe('tactus', () => {
+  calculateTactus(true);
+  bench(
+    '+tactus',
+    () => {
+      pat64.iter(64).fast(64).firstCycle();
+    },
+    { time: 1000 },
+  );
+
+  calculateTactus(false);
+  bench(
+    '-tactus',
+    () => {
+      pat64.iter(64).fast(64).firstCycle();
+    },
+    { time: 1000 },
+  );
+});
+
+describe('stack', () => {
+  calculateTactus(true);
+  bench(
+    '+tactus',
+    () => {
+      stack(pat64, pat64, pat64, pat64, pat64, pat64, pat64, pat64).fast(64).firstCycle();
+    },
+    { time: 1000 },
+  );
+
+  calculateTactus(false);
+  bench(
+    '-tactus',
+    () => {
+      stack(pat64, pat64, pat64, pat64, pat64, pat64, pat64, pat64).fast(64).firstCycle();
+    },
+    { time: 1000 },
+  );
+});
