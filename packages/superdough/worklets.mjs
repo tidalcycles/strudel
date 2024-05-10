cba96049a3de9eb1baa9982bf5873831870b4595
Jade (Rose) Rowland <jaderowlanddev@gmail.com>
2024-05-10 00:03:52 -0400

    fixed duplicate variable

--- packages/superdough/worklets.mjs
+++ packages/superdough/worklets.mjs
@@ -130,7 +130,6 @@ class AMProcessor extends AudioWorkletProcessor {
 }
 registerProcessor('am-processor', AMProcessor);
 
-const blockSize = 128;
 class CoarseProcessor extends AudioWorkletProcessor {
   static get parameterDescriptors() {
     return [{ name: 'coarse', defaultValue: 1 }];
