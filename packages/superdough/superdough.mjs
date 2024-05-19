b8085e9dc36a24c5ad3b31e10716d0411f04d771
Jade (Rose) Rowland <jaderowlanddev@gmail.com>
2024-05-19 13:25:56 -0400

    change fanchor to 0

--- packages/superdough/superdough.mjs
+++ packages/superdough/superdough.mjs
@@ -289,7 +289,7 @@ export const superdough = async (value, t, hapDuration) => {
     density = 0.03,
     // filters
     ftype = '12db',
-    fanchor = 0.5,
+    fanchor = 0,
     // low pass
     cutoff,
     lpenv,
