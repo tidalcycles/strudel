8c73ad088a6020603c955e669fe362a3aa77e621
Jade (Rose) Rowland <jaderowlanddev@gmail.com>
2024-06-02 12:32:54 -0400

    support aac

--- website/src/repl/files.mjs
+++ website/src/repl/files.mjs
@@ -76,7 +76,7 @@ export const walkFileTree = (node, fn) => {
 };
 
 export const isAudioFile = (filename) =>
-  ['wav', 'mp3', 'flac', 'ogg', 'm4a'].includes(filename.split('.').slice(-1)[0]);
+  ['wav', 'mp3', 'flac', 'ogg', 'm4a', 'aac'].includes(filename.split('.').slice(-1)[0]);
 
 function uint8ArrayToDataURL(uint8Array) {
   const blob = new Blob([uint8Array], { type: 'audio/*' });
