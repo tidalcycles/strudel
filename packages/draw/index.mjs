f0ddb2d595ea97744aeaa39f3a379bcf5dbb44e3
Tristan de Cacqueray <tristan@midirus.com>
2024-09-28 14:56:15 -0400

    Add initial shader module
    This change adds the `loadShader` and `shader` function, to be used like this:
    
    ```strudel
    await loadShader`
      // The modulation targets
      uniform float iColor;
    
      void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
        vec2 uv = fragCoord / iResolution.xy;
        vec3 col = 0.5 + 0.5*cos(iColor+uv.xyx+vec3(0,2,4));
        fragColor = vec4(col, 0);
      }
    `
    
    $: s("bd").shader({uniform: 'iColor'})
    ```

--- packages/draw/index.mjs
+++ packages/draw/index.mjs
@@ -2,5 +2,6 @@ export * from './animate.mjs';
 export * from './color.mjs';
 export * from './draw.mjs';
 export * from './pianoroll.mjs';
+export * from './shader.mjs';
 export * from './spiral.mjs';
 export * from './pitchwheel.mjs';
