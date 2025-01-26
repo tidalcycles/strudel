f4b87531715597bd7542d7dce803d2ea922a37c4
Felix Roos <flix91@gmail.com>
2025-01-26 14:58:23 +0100

    fix: tailwind setup for real now

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
 
 import remarkToc from 'remark-toc';
 import rehypeSlug from 'rehype-slug';
