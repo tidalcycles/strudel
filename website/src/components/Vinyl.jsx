6b1e6cc7afe10faf942866fde565cfa099268b1f
alex <alex@slab.org>
2025-03-01 16:53:13 +0000

    try to make more reactish

--- website/src/components/Vinyl.jsx
+++ website/src/components/Vinyl.jsx
@@ -3,7 +3,7 @@ Vinyl.jsx - <short description TODO>
 Copyright (C) 2025 Strudel contributors - see <https://github.com/tidalcycles/strudel/blob/main/repl/src/App.js>
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-
+import React, { useState, useEffect } from "react";
 import { setInterval, clearInterval } from 'worker-timers';
 import { NeoCyclist } from '@strudel/core/neocyclist.mjs';
 import { getAudioContext } from '@strudel/webaudio';
@@ -17,35 +17,32 @@ const schedulerOptions = {
   clearInterval,
   // beforeStart,
 };
-// const cyclist = new NeoCyclist(schedulerOptions);
-
+const cyclist = new NeoCyclist(schedulerOptions);
 
 export function Vinyl() {
-  const record = document.getElementById('record');
-  const cyclist = new NeoCyclist(schedulerOptions);
-  if (record) {
-    record.onclick = () => {
-      if (!cyclist.started) {
-        cyclist.start();
-      }
-      console.log('aha', cyclist.getTime(), cyclist.cycle);
+  const [isActive, setIsActive] = useState(false);
+  const [cyclepos, setCyclepos] = useState(0);
+  const activate = () => setIsActive(true);
+
+  if (isActive) {
+    if (!cyclist.started) {
+      cyclist.start();
       cyclist.setPattern(saw.segment(16));
-    };
+    }
   }
 
-  let ticks = 0;
-  const timer = setInterval(function () {
-    const record = document.getElementById('record');
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCyclepos(cyclist.cycle);
+    }, 20);
+    return () => clearInterval(intervalId);
+  }, []);
 
-    if (cyclist && cyclist.started) {
-      // console.log(cyclist.cycle);
-      const deg = (cyclist.cycle % 1) * 360;
-      record.style.webkitTransform = 'rotate(' + deg + 'deg)';
-      record.style.mozTransform = 'rotate(' + deg + 'deg)';
-      record.style.msTransform = 'rotate(' + deg + 'deg)';
-      record.style.oTransform = 'rotate(' + deg + 'deg)';
-      record.style.transform = 'rotate(' + deg + 'deg)';
-    }
+  const deg = (cyclepos % 1) * 360;
 
-  }, 20);
+  const style = {
+    fontSize: "20em",
+    transform: 'rotate(' + deg + 'deg)'
+  };
+  return <div onClick={activate} style={style}><center>💿</center></div>
 }
