3ee7d0cd4f7f53f1c9985c7fa9386c8728d044b0
Jade (Rose) Rowland <jaderowlanddev@gmail.com>
2025-04-27 16:21:14 -0400

    lint

--- website/src/repl/idbutils.mjs
+++ website/src/repl/idbutils.mjs
@@ -56,20 +56,21 @@ export function registerSamplesFromDB(config = userSamplesDBConfig, onComplete =
 
             return blobToDataUrl(blob).then((soundPath) => {
               const titlePathMap = sounds.get(parentDirectory) ?? new Map();
-          
-              titlePathMap.set(title,soundPath)
-               
+
+              titlePathMap.set(title, soundPath);
+
               sounds.set(parentDirectory, titlePathMap);
               return;
             });
           }),
       )
         .then(() => {
-        
           sounds.forEach((titlePathMap, key) => {
-            const value = Array.from(titlePathMap.keys()).sort((a,b) => {
-            return  a.localeCompare(b)
-            }).map(title => titlePathMap.get(title));
+            const value = Array.from(titlePathMap.keys())
+              .sort((a, b) => {
+                return a.localeCompare(b);
+              })
+              .map((title) => titlePathMap.get(title));
 
             registerSound(key, (t, hapValue, onended) => onTriggerSample(t, hapValue, onended, value), {
               type: 'sample',
